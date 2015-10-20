829192acccfe2cd4a1d09aadc464598c07e52fea
Justin Reyes <justin.regi.reyes@gmail.com>
2015-10-20 14:49:23 -0700

    added goroutine func

---
+++ golang/src/github.com/larskris/goroutine/goroutine.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(s)
+		fmt.Println(time.Now().Second())
+		
+		
+	}
+}
+
+func main() {
+	go say("also running")
+	say("running") 
+}
